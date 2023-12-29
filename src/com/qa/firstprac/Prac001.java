3464f5bdbac46e40cc0c386945e6e3da08ed5aef
unknown <shresthasujataqa@gmail.com>
2023-12-29 12:52:21 -0600

    this is my first commit

---
+++ src/com/qa/firstprac/Prac001.java
@@ -0,0 +1,12 @@
+package com.qa.firstprac;
+
+public class Prac001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing");
+		System.out.println("Aarvin");
+		System.out.println("Sebrina");
+	}
+
+}

e84a1b9fe559654f22c16b98a2150b3f27ccd2cf
unknown <shresthasujataqa@gmail.com>
2023-12-29 17:57:41 -0600

    branch test

---
+++ src/com/qa/firstprac/branchprac001.java
@@ -0,0 +1,10 @@
+package com.qa.firstprac;
+
+public class branchprac001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Branch test");
+	}
+
+}

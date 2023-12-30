ea96a333814d96cde415e0dc3844040e9c006ab1
sujataqa <155105121+sujataqa@users.noreply.github.com>
2023-12-29 18:07:58 -0600

    Update branchprac001.java
    testing from github

--- src/com/qa/firstprac/branchprac001.java
+++ src/com/qa/firstprac/branchprac001.java
@@ -5,6 +5,8 @@ public class branchprac001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Branch test");
+		System.out.println("Branch testing from github");
+		
 	}
 
 }
